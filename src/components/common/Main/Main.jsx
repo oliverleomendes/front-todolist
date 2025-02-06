1ff9a3cd09c5e3bbbb3ffd973c54cf58d4b09056
LEONARDO MENDES DE OLIVEIRA <leonardo.moliveira25@senacsp.edu.br>
2025-02-05 21:57:22 -0300

    Criação da autenticação de usuários

--- src/components/common/Main/Main.jsx
+++ src/components/common/Main/Main.jsx
@@ -1,13 +1,13 @@
 import { Component } from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "../../pages/Home";
+import Login from "../../pages/Login";
 
 export default class Main extends Component{
     render(){
         return(
             <main>
                 <Routes>
-                    <Route path="/" element={<Home />} />
+                    <Route path="/" element={<Login />} />
                 </Routes>
             </main>
         )
---
+++ src/components/pages/Login.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import "./Login.css";
+import axios from 'axios';
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        
+        await axios.post('http://localhost:4000/usuario/autenticar', {
+            "email": username,
+            "senha": password
+        }).then(response => {
+            console.log(response.data)
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error)
+        })
+        
+    }
+
+    return(
+        <>
+            <div className="container mt-5 mb-5 py-4">
+                <form onSubmit={handleSubmit}>
+                    <div className="d-flex justify-content-center">
+                        <div className="card">
+                            <div className="card-header">
+                                <h5 className="text-center">Autenticação</h5>
+                            </div>
+                            <div className="card-body">
+                                <div className="row">
+                                    <div className="col-md-12">
+                                        <label htmlFor="">Usuário:</label>
+                                        <input type="text" name="user" id="user" className="form-control" onChange={(e) => setUsername(e.target.value)} />
+                                    </div>
+                                </div>      
+                                <div className="row">
+                                    <div className="col-md-12">
+                                        <label htmlFor="">Senha:</label>
+                                        <input type="password" name="password" id="password" className="form-control" onChange={(e) => setPassword(e.target.value)} />
+                                    </div>
+                                </div>  
+                                <div className="row mt-2 mb-2">
+                                    <div className="d-flex justify-content-center">
+                                        <button type="submit" className="btn btn-success">
+                                            Autenticar
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>      
+                    </div>
+                </form>
+            </div>
+        </>
+    )
+    
+}
+
+export default Login
